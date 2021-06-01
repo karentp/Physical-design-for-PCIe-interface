5edc8eef953c0df70422eabac9d6ba60fa0cb7fd
felipebm09 <felipebm09@gmail.com>
2021-06-01 02:27:20 -0600

    Versión de Serie-Paralelo (NO FUNCIONA ESTRUCTURAL)

--- Serial-Paralelo/BancoPruebasSerialParalelo.v
+++ Serial-Paralelo/BancoPruebasSerialParalelo.v
@@ -1,33 +1,70 @@
-`include "probador_serial.v"
-`include "serial_paralelo.v"
+`include "Probador_SerialParalelo.v"
+`include "SerialParalelo.v"
+`include "cmos_cells.v"
+`include "SerialParalelo_estruct.v"
 
 module BancoPruebasSerialParalelo;
 
-wire clk_4f, clk_32f;
-wire [7:0] data2send;
-wire [3:0] BC_counter;
-wire active, valid_out, data_in;
+/*AUTOWIRE*/
+// Beginning of automatic wires (for undeclared instantiated-module outputs)
+wire			active;			// From conductual of SerialParalelo.v
+wire			active_estruct;		// From estructural of SerialParalelo_estruct.v
+wire			clk_32f;		// From probador of Probador_SerialParalelo.v
+wire			clk_4f;			// From probador of Probador_SerialParalelo.v
+wire [7:0]		data2send;		// From conductual of SerialParalelo.v
+wire [7:0]		data2send_estruct;	// From estructural of SerialParalelo_estruct.v
+wire			data_in;		// From probador of Probador_SerialParalelo.v
+wire [7:0]		data_out;		// From conductual of SerialParalelo.v
+wire [7:0]		data_out_estruct;	// From estructural of SerialParalelo_estruct.v
+wire			valid_out;		// From conductual of SerialParalelo.v
+wire			valid_out_estruct;	// From estructural of SerialParalelo_estruct.v
+// End of automatics
 
-probador_serial probador(.clk_4f    (clk_4f), 
-                                 .clk_32f   ( clk_32f),
-                                 .data_in   (data_in),
-                                 .data2send (data2send),
-                                 .BC_counter    (BC_counter),
-                                 .active    (active),
-                                 .valid_out     (valid_out));
+Probador_SerialParalelo probador(/*AUTOINST*/
+				 // Outputs
+				 .clk_4f		(clk_4f),
+				 .clk_32f		(clk_32f),
+				 .data_in		(data_in),
+				 // Inputs
+				 .data2send		(data2send[7:0]),
+				 .data_out		(data_out[7:0]),
+				 .data2send_estruct	(data2send_estruct[7:0]),
+				 .data_out_estruct	(data_out_estruct[7:0]),
+				 .active		(active),
+				 .valid_out		(valid_out),
+				 .active_estruct	(active_estruct),
+				 .valid_out_estruct	(valid_out_estruct));
 
 
-serial_paralelo SP(.clk_4f  (clk_4f), 
-                   .clk_32f (clk_32f),
-                   .data_in (data_in),
-                   .data2send   (data2send),
-                   .BC_counter  (BC_counter),
-                   .active  (active),
-                   .valid_out   (valid_out));
+SerialParalelo conductual(/*AUTOINST*/
+			  // Outputs
+			  .data2send		(data2send[7:0]),
+			  .data_out		(data_out[7:0]),
+			  .active		(active),
+			  .valid_out		(valid_out),
+			  // Inputs
+			  .clk_4f		(clk_4f),
+			  .clk_32f		(clk_32f),
+			  .data_in		(data_in));
+
+
+SerialParalelo_estruct estructural (/*AUTOINST*/
+				    // Outputs
+				    .active_estruct	(active_estruct),
+				    .data2send_estruct	(data2send_estruct[7:0]),
+				    .data_out_estruct	(data_out_estruct[7:0]),
+				    .valid_out_estruct	(valid_out_estruct),
+				    // Inputs
+				    .clk_32f		(clk_32f),
+				    .clk_4f		(clk_4f),
+				    .data_in		(data_in));
+
+
+
+
+
 
 endmodule
-
-
 // Local Variabes:
     // eval:(verilog-read-defines)
     // eval:(verilog-read-includes)
--- Serial-Paralelo/Probador_SerialParalelo.v
+++ Serial-Paralelo/Probador_SerialParalelo.v
@@ -1,7 +1,8 @@
-module probador_serial(
+module Probador_SerialParalelo(
     input [7:0] data2send, data_out,
-    input [3:0] BC_counter,
+    input [7:0] data2send_estruct, data_out_estruct,
     input active, valid_out,
+    input active_estruct, valid_out_estruct,
 
     output reg clk_4f, clk_32f, data_in);
 
@@ -111,6 +112,36 @@ module probador_serial(
         @(posedge clk_32f)
         {data_in} = 0;
 
+
+
+//CUARTO BC*************************************
+        @(posedge clk_32f)
+        {data_in} = 1;
+
+        @(posedge clk_32f)
+        {data_in} = 0;
+
+        @(posedge clk_32f)
+        {data_in} = 1;
+
+        @(posedge clk_32f)
+        {data_in} = 1;
+
+        @(posedge clk_32f)
+        {data_in} = 1;
+
+        @(posedge clk_32f)
+        {data_in} = 1;
+
+        @(posedge clk_32f)
+        {data_in} = 0;    
+
+        @(posedge clk_32f)
+        {data_in} = 0;
+
+
+
+
 //PRIMER FF*********************************
 
         @(posedge clk_32f)
@@ -167,15 +198,20 @@ module probador_serial(
 
         @(posedge clk_32f)
 
+        @(posedge clk_4f)
+        @(posedge clk_4f)
+        @(posedge clk_4f)
+        @(posedge clk_4f)
+
         $finish;
 
     end
 
-    initial clk_4f <=0;
-    initial clk_32f <= 0;
+    initial clk_4f <= 0;
+    initial clk_32f <= 1;
 
-    always #64 clk_4f <= ~clk_4f;
-    always #8 clk_32f <= ~clk_32f;
+    always #16 clk_4f <= ~clk_4f;
+    always #2 clk_32f <= ~clk_32f;
 
 
 endmodule
